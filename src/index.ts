4357cdabc581a98ffb9f1bdab4c7fbe4f9865d92
uuronn <uuronprogram@gmail.com>
2024-11-24 03:18:56 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,65 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import type {
+	MessageAPIResponseBase,
+	TextMessage,
+	WebhookEvent,
+} from "@line/bot-sdk";
+import { Hono } from "hono";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	CHANNEL_ACCESS_TOKEN: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+app.get("*", (c) => c.text("Hello World!"));
+
+app.post("/api/webhook", async (c) => {
+	const data = await c.req.json();
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const events: WebhookEvent[] = (data as any).events;
+	const accessToken: string = c.env.CHANNEL_ACCESS_TOKEN;
+
+	await Promise.all(
+		events.map(async (event: WebhookEvent) => {
+			try {
+				await textEventHandler(event, accessToken);
+			} catch (err: unknown) {
+				if (err instanceof Error) {
+					console.error(err);
+				}
+				return c.json({
+					status: "error",
+				});
+			}
+		}),
+	);
+	return c.json({ message: "ok" });
+});
+
+const textEventHandler = async (
+	event: WebhookEvent,
+	accessToken: string,
+): Promise<MessageAPIResponseBase | undefined> => {
+	if (event.type !== "message" || event.message.type !== "text") {
+		return;
+	}
+
+	const { replyToken } = event;
+	const { text } = event.message;
+	const response: TextMessage = {
+		type: "text",
+		text,
+	};
+	await fetch("https://api.line.me/v2/bot/message/reply", {
+		body: JSON.stringify({
+			replyToken: replyToken,
+			messages: [response],
+		}),
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+			"Content-Type": "application/json",
+		},
+	});
+};
+
+export default app;
